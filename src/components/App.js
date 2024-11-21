075fdf3a25a03300716ec13d1a00a43b6db397df
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-11-21 15:26:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,25 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState(''); // State for the name
+
+    const onGreet = (e) => {
+        setName(e.target.value);
+    };
   return (
     <div>
         {/* Do not remove the main div */}
+      <div>
+            <label htmlFor="greet">Enter your name:</label><br />
+              <input
+                    type="text"
+                    id="greet"
+                    name="name"
+                    onChange={onGreet}
+                    value={name}
+                />
+            </div>
+            <p> {name ? `Hello ${name}!` : ''}</p>    
     </div>
   )
 }
